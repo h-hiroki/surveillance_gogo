62564d3a7213e4b86c4b46af78403d523decc314
h-hiroki <gyouzakuuka@gmail.com>
2019-04-21 22:56:18 +0900

    ⛳️React側からAPIをコールして結果を表示するところまで

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
+const HEALTH_CHECK_ENDPOINT = ' http://localhost:1323/health_check';
+
+interface IProps {}
+interface IState {
+  healthCheck: string,
+}
+
+export default class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      healthCheck: ""
+    }
+  }
+
+  handleHealthCheck() {
+    axios
+      .get(HEALTH_CHECK_ENDPOINT)
+      .then((results) => {
+        this.setState({healthCheck: results.data});
+      })
+      .catch(() => {
+        this.setState({healthCheck: "failed health check"});
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,10 +46,15 @@ class App extends Component {
           >
             Learn React
           </a>
+          <br/>
+          <p>{this.state.healthCheck}</p>
+          <input
+            type="button"
+            value="Check Health"
+            onClick={() => this.handleHealthCheck()}
+          />
         </header>
       </div>
     );
   }
 }
-
-export default App;
