c393f6bee4b115f077af7b5a9f06f93bbbda82a2
h-hiroki <gyouzakuuka@gmail.com>
2019-04-23 21:35:59 +0900

    ⛳️HTMLを取得してハッシュ化するところまで

--- server/app/crawler/main.go
+++ server/app/crawler/main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"crypto/sha256"
 )
 
 func CheckDiff() {
@@ -15,4 +16,13 @@ func CheckDiff() {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	log.Println(string(body)) // HTMLが取得できていることを確認する
+
+	hash := convertIntoHash(string(body))
+	log.Println(string(hash[:])) // ハッシュ値を確認する
+	// TODO 取得したHTMLとハッシュ値が文字化けしているので修正すること。
+}
+
+// sha256でハッシュ化する
+func convertIntoHash(stringHTML string) [32]byte {
+	return sha256.Sum256([]byte(stringHTML))
 }
